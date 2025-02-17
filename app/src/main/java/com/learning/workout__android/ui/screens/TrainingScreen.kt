12ba4183cdb2f7826a01c34837d9033c7fb989da
PavlenkoSt <verbatim8872@gmail.com>
2025-02-17 21:28:16 +0200

    Training - exercise item UI

--- app/src/main/java/com/learning/workout__android/ui/screens/TrainingScreen.kt
+++ app/src/main/java/com/learning/workout__android/ui/screens/TrainingScreen.kt
@@ -3,20 +3,25 @@ package com.learning.workout__android.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
@@ -30,6 +35,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -157,12 +163,18 @@ private fun TrainingExerciseList(
         // Update the list
     }
 
-    LazyColumn(state = lazyListState) {
-        items(exercisesList, key = { it.id }) {
-            ReorderableItem(reorderableLazyListState, key = it.id) { isDragging ->
+    LazyColumn(
+        state = lazyListState,
+        contentPadding = PaddingValues(all = 8.dp)
+    ) {
+        itemsIndexed(exercisesList, key = { _, item -> item.id }) { idx, item ->
+            ReorderableItem(reorderableLazyListState, key = item.id) { isDragging ->
                 ExerciseItem(
-                    exercise = it,
-                    modifier = Modifier.draggableHandle()
+                    exercise = item,
+                    modifier = Modifier
+                        .draggableHandle()
+                        .fillMaxWidth(),
+                    idx = idx
                 )
             }
         }
@@ -172,10 +184,55 @@ private fun TrainingExerciseList(
 @Composable
 private fun ExerciseItem(
     exercise: ExerciseModel,
+    idx: Int,
     modifier: Modifier = Modifier
 ) {
-    Row(modifier = modifier) {
-        Text(text = exercise.exercise)
+    Column {
+        Card(modifier = modifier) {
+            Text(
+                text = "${idx + 1}. ${exercise.exercise}",
+                modifier = Modifier.padding(
+                    top = 8.dp,
+                    start = 8.dp,
+                    end = 8.dp
+                )
+            )
+            Row(
+                modifier = Modifier.padding(horizontal = 4.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Button(
+                    onClick = {},
+                    modifier = Modifier.weight(1f),
+                    shape = ShapeDefaults.Medium
+                ) {
+                    Text("-")
+                }
+                Spacer(modifier = Modifier.width(16.dp))
+                Column(
+                    modifier = Modifier.width(80.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text("0/1")
+                    LinearProgressIndicator(
+                        progress = { 0.5f },
+                        modifier = Modifier.fillMaxWidth(),
+                        trackColor = MaterialTheme.colorScheme.onPrimary,
+                        color = MaterialTheme.colorScheme.primary,
+                        strokeCap = StrokeCap.Round,
+                    )
+                }
+                Spacer(modifier = Modifier.width(16.dp))
+                Button(
+                    onClick = {},
+                    modifier = Modifier.weight(1f),
+                    shape = ShapeDefaults.Medium
+                ) {
+                    Text("+")
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(8.dp))
     }
 }
 
