00d121c26e000719580aedf35d2223468988d7c5
PavlenkoSt <verbatim8872@gmail.com>
2026-05-12 17:38:38 +0300

    increment version code, fix rc api key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.stanislav_pav.repstation"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
