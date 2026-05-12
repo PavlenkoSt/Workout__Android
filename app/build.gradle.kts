3f31a9fbd60d7bb7d4282c2f0f5d7010006819f2
PavlenkoSt <verbatim8872@gmail.com>
2026-05-12 13:50:06 +0300

    chore: update Gradle and plugin configurations
    - Upgraded Android Gradle Plugin version from 8.13.1 to 9.1.1 for improved features and compatibility.
    - Updated KSP plugin version from 2.2.21-2.0.5 to 2.3.2 for enhanced functionality.
    - Modified Gradle wrapper to use version 9.3.1 for better performance.
    - Added new properties in gradle.properties for Android build features and SDK management.
    - Introduced gradle-daemon-jvm.properties for toolchain configuration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
-    id("com.google.devtools.ksp") version "2.2.21-2.0.5"
+    id("com.google.devtools.ksp") version "2.3.2"
 }
 
 android {
